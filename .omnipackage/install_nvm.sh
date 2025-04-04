151dd103aa4150af3469202f06921874f1b1901d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-04-04 15:23:39 +0200

    enable ppm build

--- .omnipackage/install_nvm.sh
+++ .omnipackage/install_nvm.sh
@@ -10,3 +10,20 @@ if nvm --version; then
 fi
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# node-gyp (during yarn build:apm) fails with Python versions below 3.9
+if python3 --version; then
+  minor_version=$(python3 -c 'import sys; print(sys.version_info.minor)')
+  minor_version=$(($minor_version+0))
+  if (( $minor_version > 9)); then
+    exit 0
+  fi
+fi
+curl -L https://www.python.org/ftp/python/3.13.2/Python-3.13.2.tar.xz | tar -Jx -C .
+cd Python-3.13.2
+./configure --disable-test-modules
+make -j$(nproc)
+make install
+cd ..
+rm -rf Python-3.13.2
+pip3 install setuptools
